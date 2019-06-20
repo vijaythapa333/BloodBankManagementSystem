5feb1c5cf40b61b39b8d75bd35ba76d5f719f307
Vijay Thapa <vijaythapa333@gmail.com>
2019-06-20 07:40:03 +0545

    Created userBLL

---
+++ BloodBankManagementSystem/BLL/userBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankManagementSystem.BLL
+{
+    class userBLL
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string full_name { get; set; }
+        public string contact { get; set; }
+        public string address { get; set; }
+        public DateTime added_date { get; set; }
+        public string image_name { get; set; }
+    }
+}
