4a38df498fad8c771b33b6f7f03d02f313430c92
Vijay Thapa <vijaythapa333@gmail.com>
2019-07-16 07:34:12 +0545

    Created BLL for Donor Module

---
+++ BloodBankManagementSystem/BLL/donorBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankManagementSystem.BLL
+{
+    class donorBLL
+    {
+        public int donor_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string contact { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string blood_group { get; set; }
+        public DateTime added_date { get; set; }
+        public string image_name { get; set; }
+        public int added_by { get; set; }
+    }
+}
