8ecc56e45cc48b3f50705204d71cc614ee346ed4
Vijay Thapa <vijaythapa333@gmail.com>
2019-07-16 08:24:14 +0545

    Created donorDAL

---
+++ BloodBankManagementSystem/DAL/donorDAL.cs
@@ -0,0 +1,231 @@
+﻿using BloodBankManagementSystem.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodBankManagementSystem.DAL
+{
+    class donorDAL
+    {
+        //Create a Connection String to Connect Database
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        #region SELECT to display data in DataGridView from database
+        public DataTable Select()
+        {
+            // Create object to DataTAble to hold the data from database and return it
+            DataTable dt = new DataTable();
+
+            //Create object of SQL Connection to Connect DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write SQL Query to SElect the DAta from DAtabase
+                string sql = "SELECT * FROM tbl_donors";
+
+                //Create the SQlCommand to Execute the Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create SQl DAta Adapter to Hold the Data Temporarily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //open Database Connection
+                conn.Open();
+
+                //Pass the Data from adapter to DataTable
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                //Display Message if there's any Exceptional Errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close Database Connection
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+        #region INSERT data to database
+        public bool Insert(donorBLL d)
+        {
+            //Create a Boolean Variable and SEt its default value to false
+            bool isSuccess = false;
+
+            //Create SqlConnection to Connect Database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write the Query to INSERT data into database
+                string sql = "INSERT INTO tbl_donors (first_name, last_name, email, contact, gender, address, blood_group, added_date, image_name, added_by) VALUES (@first_name, @last_name, @email, @contact, @gender, @address, @blood_group, @added_date, @image_name, @added_by)";
+
+                //Create SQL Command to Execute the Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Pass the value to SQL Query
+                cmd.Parameters.AddWithValue("@first_name", d.first_name);
+                cmd.Parameters.AddWithValue("@last_name", d.last_name);
+                cmd.Parameters.AddWithValue("@email", d.email);
+                cmd.Parameters.AddWithValue("@contact", d.contact);
+                cmd.Parameters.AddWithValue("@gender", d.gender);
+                cmd.Parameters.AddWithValue("@address", d.address);
+                cmd.Parameters.AddWithValue("@blood_group", d.blood_group);
+                cmd.Parameters.AddWithValue("@added_date", d.added_date);
+                cmd.Parameters.AddWithValue("@image_name", d.image_name);
+                cmd.Parameters.AddWithValue("@added_by", d.added_by);
+
+                //Open DAtabase Connection
+                conn.Open();
+
+                //Create an Integer Variable to Check whether the query was executed Successfully or Not
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the Query is Executed Successfully the value of rows willb e greater than Zero else it will be Zero
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                //Display Error Message if there's any Exceptional Errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //CLose Database Connection
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region UPDATE donors in DAtabase
+        public bool Update(donorBLL d)
+        {
+            //Create a Boolean Variable and SEt its Default Value to FAlse
+            bool isSuccess = false;
+            //Create SQLConnection to Connect DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Create a SQL Query to Update Donors
+                string sql = "UPDATE tbl_donors SET first_name=@first_name, last_name=@last_name, email=@email, contact=@contact, gender=@gender, address=@address, blood_group=@blood_group, image_name=@image_name, added_by=@added_by WHERE donor_id=@donor_id";
+
+                //Create SQL Command Here
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Pass the Value to SQL Query
+                cmd.Parameters.AddWithValue("@first_name", d.first_name);
+                cmd.Parameters.AddWithValue("@last_name", d.last_name);
+                cmd.Parameters.AddWithValue("@email", d.email);
+                cmd.Parameters.AddWithValue("@contact", d.contact);
+                cmd.Parameters.AddWithValue("@gender", d.gender);
+                cmd.Parameters.AddWithValue("@address", d.address);
+                cmd.Parameters.AddWithValue("@blood_group", d.blood_group);
+                cmd.Parameters.AddWithValue("@image_name", d.image_name);
+                cmd.Parameters.AddWithValue("@added_by", d.added_by);
+                cmd.Parameters.AddWithValue("@donor_id", d.donor_id);
+
+                //Open Database Connection
+                conn.Open();
+
+                //Create an Integer Variable to check whether the query executed Successfully or not
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the Query is Executed Successfully then its value will be greater than 0 else it will be 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                //Display Error Message if there's any exceptional errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //CLose Database Connection
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region DELETE donros from Database
+        public bool Delete(donorBLL d)
+        {
+            //Create a Boolean variable and set its default value to false
+            bool isSuccess = false;
+            //Create SqlConnection To Connect DAtabase
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write the Query to Delete Donors from Database
+                string sql = "DELETE FROM tbl_donors WHERE donor_id=@donor_id";
+
+                //Create SQL Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Pass the Value to Sql Query using Parameters
+                cmd.Parameters.AddWithValue("@donor_id", d.donor_id);
+
+                //Open Database Connection
+                conn.Open();
+
+                //Create an Integer VAriable to Check whether the query executed Successfully or Not
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the Query executed succesfully then the value of rows will be greater than 0 else it will be 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //FAiled to Exeute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                //Display Error Message if there's any Exceptional Errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //CLose Database Connection
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    }
+}
